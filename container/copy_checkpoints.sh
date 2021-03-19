511cb5763b098d6e4efbd611ec52ded34cb5783d
toshiki tomihira <toshiki19970714@gmail.com>
2021-03-20 00:37:40 +0900

    Change to python

--- container/copy_checkpoints.sh
+++ container/copy_checkpoints.sh
@@ -3,6 +3,5 @@ mkdir -p ./outputs
 while true
 do
   mv ./outputs/*/*/checkpoints/checkpoint*.pt /opt/ml/checkpoints/
-  #aws s3 sync ./outputs s3://sagemaker-us-east-1-067307027894/checkpoints/ 
   sleep 60
 done
